839e826a48e1dba10a360779c4f0f3f3da84033c
Erin Painting <27023931@sun.ac.za>
2026-06-21 20:54:21 +0200

    route guard added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { Login } from './components/login/login';
 import { Register } from './components/register/register';
 import { Canvas } from './components/canvas/canvas';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: Login },
   { path: 'register', component: Register },
-  { path: 'canvas', component: Canvas },
+  { path: 'canvas', component: Canvas, canActivate: [authGuard] },
 ];
