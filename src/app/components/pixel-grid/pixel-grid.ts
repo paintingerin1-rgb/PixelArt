8ea2b925bffd1a49da1351666930c526dd5f1dca
Erin Painting <27023931@sun.ac.za>
2026-06-23 10:37:58 +0200

    adding pixel component

--- src/app/components/pixel-grid/pixel-grid.ts
+++ src/app/components/pixel-grid/pixel-grid.ts
@@ -1,9 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-pixel-grid',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './pixel-grid.html',
   styleUrl: './pixel-grid.css',
 })
-export class PixelGrid {}
+export class PixelGrid {
+  gridSize: number = 16;
+  grid: string[][] = [];
+
+  constructor() {
+    for (let index = 0; index < this.gridSize; index++) {
+      const row: string[] = [];
+      for (let j = 0; j < this.gridSize; j++) {
+        row.push('#FFFFFF');
+      }
+      this.grid.push(row);
+    }
+  }
+  onPixelClick(rowIndex: number, colIndex: number) {
+    const newColor = this.grid[rowIndex][colIndex] === '#FFFFFF' ? '#000000' : '#FFFFFF';
+    this.grid[rowIndex][colIndex] = newColor;
+  }
+}
